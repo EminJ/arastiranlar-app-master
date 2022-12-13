0b6da1e0964bb85c0994d91129dcb2d1ae5ae8a1
Muhammet Emin Güleşci <114227722+EminJ@users.noreply.github.com>
2022-12-13 03:24:26 +0300

    up

--- src/app.js
+++ src/app.js
@@ -2,30 +2,33 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const session = require('express-session');
+require('dotenv').config();
 
 const app = express();
-var whitelist = ['https://loose-luminosity.vercel.app','76.76.21.164']
-var corsOptions = {
-     origin: (origin, callback) => {
-          if (whitelist.indexOf(origin) !== -1)
-               callback(null, true);
-          else
-               callback(new Error("! ! !"));
-     }
-}
+app.use(
+     cors({
+          origin: "*",
+     })
+);
 app.use(bodyParser.json());
-app.use(bodyParser.json({limit: '10mb'}));
-app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
+app.use(bodyParser.json({
+     limit: '10mb'
+}));
+app.use(bodyParser.urlencoded({
+     limit: '10mb',
+     extended: true
+}));
 app.use(session({
-    secret: 'secret-key',
-    resave: false,
-    saveUninitialized: true,
-    secure: true,
-    cookie: { secure: true }
-    }));
-
-app.get('/',cors(corsOptions), (req,res)=>{
-    res.send('s..s');
+     secret: process.env.SECRET,
+     resave: false,
+     saveUninitialized: true,
+     secure: true,
+     cookie: {
+          secure: true
+     }
+}));
+app.get('/', (req, res) => {
+     res.send('s..s');
 })
 
 require("./routes/auth.routes")(app);
